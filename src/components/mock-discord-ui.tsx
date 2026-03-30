30da12943a097fbfc2f11621a932726202e1fb0b
Denis-Tyvonyuk <denusa321@gmail.com>
2026-03-30 22:08:44 +0300

    md 2/3

--- src/components/mock-discord-ui.tsx
+++ src/components/mock-discord-ui.tsx
@@ -1,6 +1,22 @@
 import React, { PropsWithChildren } from "react"
 import { Icons } from "./icons"
-import { Inbox, PlusCircle, UserCircle } from "lucide-react"
+import {
+  Cog,
+  Gift,
+  Headphones,
+  HelpCircle,
+  Inbox,
+  Menu,
+  Mic,
+  Phone,
+  Pin,
+  PlusCircle,
+  Search,
+  Smile,
+  Sticker,
+  UserCircle,
+  Video,
+} from "lucide-react"
 import Image from "next/image"
 
 const MockDiscordUI = ({ children }: PropsWithChildren) => {
@@ -88,6 +104,69 @@ const MockDiscordUI = ({ children }: PropsWithChildren) => {
                   </div>
                 </div>
               ))}
+            </div>
+          </div>
+        </div>
+        <div className="p-2 bg-[#292b2f] flex items-center">
+          <div className="size-8 rounded-full bg-brand-700 mr-2" />
+          <div className="flex-1">
+            <p className="text-sm font-medium text-white">You</p>
+            <p className="text-xs text-[#b9bbbe] flex items-center">
+              @your_account
+            </p>
+          </div>
+          <div className="flex items-center space-x-2">
+            <Mic className="size-5 text-[#b9bbbe] hover:text-white cursor-pointer" />
+            <Headphones className="size-5 text-[#b9bbbe] hover:text-white cursor-pointer" />
+            <Cog className="size-5 text-[#b9bbbe] hover:text-white cursor-pointer" />
+          </div>
+        </div>
+      </div>
+      <div className="flex flex-1 flex-col">
+        <div className="h-16 bg-[#36393f] flex items-center px-4 shadow-sm border-b border-[#202225]">
+          <div className="md:hidden mr-4">
+            <Menu className="size-6 text-[#b9bbbe] hover:text-white cursor-pointer" />
+          </div>
+          <div className="flex items-center ">
+            <div className="relative">
+              <Image
+                src={"/brand-asset-profile-picture.png"}
+                alt="PingPanda Avatar"
+                width={40}
+                height={40}
+                className="object-cover rounded-full mr-3"
+              />
+              <div className="absolute bottom-0 right-3 size-3 bg-green-500 rounded-full border-2 border-[#36393f]" />
+            </div>
+            <p className="font-semibold text-white">PingPanda</p>
+          </div>
+          <div className="ml-auto flex items-center space-x-4 text-[#b9bbbe]">
+            <Phone className="size-5 hover:text-white cursor-not-allowed hidden sm:block" />
+            <Video className="size-5 hover:text-white cursor-not-allowed hidden sm:block" />
+            <Pin className="size-5 hover:text-white cursor-not-allowed hidden sm:block" />
+            <UserCircle className="size-5 hover:text-white cursor-not-allowed hidden sm:block" />
+            <Search className="size-5 hover:text-white cursor-not-allowed hidden sm:block" />
+            <Inbox className="size-5 hover:text-white cursor-not-allowed hidden sm:block" />
+            <HelpCircle className="size-5 hover:text-white cursor-not-allowed hidden sm:block" />
+          </div>
+        </div>
+
+        <div className="flex-1 overflow-y-auto p-4 bg-discord-background flex flex-col-reverse">
+          {children}
+        </div>
+        <div className="p-4 ">
+          <div className="flex items-center bg-[#40444b] rounded-lg p-1">
+            <PlusCircle className="mx-3 text-[#b9bbbe] hover:text-white cursor-not-allowed" />
+            <input
+              readOnly
+              type="text"
+              placeholder="Message @PingPanda"
+              className="flex-1 bg-transparent py-2.5 px-1 text-white placeholder-[#72767d] focus:outline-none cursor-not-allowed"
+            />
+            <div className="flex items-center space-x-3 mx-3 text-[#b9bbbe]">
+              <Gift className="size-5 hover:text-white cursor-not-allowed hidden sm:block" />
+              <Sticker className="size-5 hover:text-white cursor-not-allowed hidden sm:block" />
+              <Smile className="size-5 hover:text-white cursor-not-allowed hidden sm:block" />
             </div>
           </div>
         </div>
