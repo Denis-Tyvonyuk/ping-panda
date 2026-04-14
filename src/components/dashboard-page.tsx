c4fb40f742df868d636f903959003e24e420ca7d
Denis-Tyvonyuk <denusa321@gmail.com>
2026-04-14 21:22:54 +0300

    dashboard 1/4

---
+++ src/components/dashboard-page.tsx
@@ -0,0 +1,33 @@
+import React, { ReactNode } from "react"
+import { Button } from "./ui/button"
+import { ArrowLeft } from "lucide-react"
+
+interface DashboardPageProps {
+  title: string
+  children?: ReactNode
+  hideBackButton?: boolean
+  cta?: ReactNode
+}
+
+const DashboardPage = ({
+  title,
+  children,
+  cta,
+  hideBackButton,
+}: DashboardPageProps) => {
+  return (
+    <section className="flex-1 h-full w-full flex flex-col">
+      <div className="p-6 sm:p-8 flex justify-between border-b border-gray-200">
+        <div className="flex flex-col sm:flex-row smLitems-center gap-y-2 gap-x-8">
+          {hideBackButton ? null : (
+            <Button className="w-fit bg-white" variant={"outline"}>
+              <ArrowLeft className="size-4" />
+            </Button>
+          )}
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default DashboardPage
